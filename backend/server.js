f2f0eaaa23a033c038e095d9d0051ce7c6a0589e
Krtyaka <krtyaka.ahuja@gmail.com>
2025-09-14 17:16:02 +0530

    simple server setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Get request successful!");
+  res.status(201).json({ message: "Get request running successfully!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
