50367d20636e5cdf7b7a51c6a6c5b969e725082f
Krtyaka <krtyaka.ahuja@gmail.com>
2025-09-16 00:54:33 +0530

    Connected the server with the database and added an error handling middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import cors from "cors";
 
 import { connectDb } from "./config/db.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   console.log("Get request successful!");
   res.status(201).json({ message: "Get request running successfully!" });
 });
